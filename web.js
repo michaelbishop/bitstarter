4947325493c728d52498f515addb4623ac831a26
Michael Bishop <michael@michaelsplace.net>
2013-07-11 04:30:28 +0000

    without buffer

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var hello=fs.readFileSync("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
